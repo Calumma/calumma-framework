45520544cbf6fb16d513f19a822ce8e8f25a1ee8
Filipe <filipe_santiago@id.uff.br>
2021-08-09 15:46:29 -0300

    Ajusta imports

--- src/core/form/index.js
+++ src/core/form/index.js
@@ -8,12 +8,12 @@ import CalummaEnumSelect from './calummaEnumSelect'
 import CalummaFileUploader from './calummaFileUploader'
 import CalummaErrorAlert from './calummaFormErrorAlert'
 import CalummaDateTimeField from './calummaDateTimeField'
-
+import CalummaNumberField from './calummaNumberField'
 
 import useForm from './useForm'
 import Yup from './yup'
 
 export { 
          CalummaForm, CalummaDateTimeField, CalummaDateField, CalummaTimeField, CalummaTextField, CalummaAutoComplete, 
-         CalummaServerSelect, CalummaEnumSelect, CalummaErrorAlert, CalummaFileUploader, useForm, Yup 
+         CalummaServerSelect, CalummaEnumSelect, CalummaNumberField, CalummaErrorAlert, CalummaFileUploader, useForm, Yup 
        }
--- src/core/index.js
+++ src/core/index.js
@@ -1,6 +1,6 @@
 import { useCrud, useSecurity } from './hooks'
 import {
-    CalummaForm, CalummaDateField, CalummaTimeField, CalummaTextField, CalummaAutoComplete, CalummaDateTimeField,
+    CalummaForm, CalummaDateField, CalummaTimeField, CalummaTextField, CalummaAutoComplete, CalummaDateTimeField, CalummaNumberField,
     CalummaServerSelect, CalummaEnumSelect, CalummaFileUploader, CalummaErrorAlert, useForm, Yup
 } from "./form"
 import Feedback, { useFeedback } from './feedback'
@@ -8,6 +8,6 @@ import DataTableTemplate, { useDatatableTransmutation } from './datatable'
 
 export {
     useCrud, useSecurity, CalummaForm, CalummaDateField, CalummaTimeField, CalummaTextField, CalummaAutoComplete, Feedback, CalummaErrorAlert,
-    CalummaDateTimeField, CalummaServerSelect, CalummaEnumSelect, CalummaFileUploader, useForm, Yup, useFeedback, 
+    CalummaDateTimeField, CalummaServerSelect, CalummaEnumSelect, CalummaNumberField, CalummaFileUploader, useForm, Yup, useFeedback, 
     DataTableTemplate as CalummaDatatable, useDatatableTransmutation
 }
